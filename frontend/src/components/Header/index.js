a5a4d7d7a44e845e4ae6e203724618e44e88f06d
Kevin Alves <kevin.bernardo64@gmail.com>
2021-08-28 15:34:13 -0300

    Calculating projects quantity

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,18 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Container, Statistics, AddProject, Plus, Button } from './styles';
 
 export function Header({projects}) {
   
-//   const [andamento, setAndamento] = useState(0);  
-//   const [encerrado, setEncerrado] = useState(0);  
+  const [andamento, setAndamento] = useState(0);  
+  const [encerrado, setEncerrado] = useState(0); 
+  const [total, setTotal]         = useState(0);  
 
   useEffect(()=>{
       function sumStatistics(){
-        projects.forEach(function(x){
-            //calcula os encerrados e em andamentos
-
-        })
+        setTotal(projects.length)
+        setEncerrado(projects.reduce((a, b) => a + b.encerrado, 0)) 
+        setAndamento(projects.reduce((a, b) => a + !b.encerrado, 0))
       }
 
     sumStatistics()
@@ -23,17 +23,17 @@ export function Header({projects}) {
       <Container>
           <Statistics>
               <div>
-                  <h2>{projects.length}</h2> 
+                  <h2>{total}</h2> 
                   Projetos ao total
               </div>
 
               <div>
-                  <h2>0</h2>
+                  <h2>{andamento}</h2>
                   Em andamento
               </div>
 
               <div>
-                  <h2>0</h2>
+                  <h2>{encerrado}</h2>
                   Encerrados
               </div>
           </Statistics>
